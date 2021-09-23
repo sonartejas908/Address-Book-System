2cf672299fff04079765a90cccece6b0f62b382a
Tejassonar <sonartejas908@gmail.com>
2021-09-23 22:01:57 +0530

    Added Welcome Message to Solution

---
+++ WelcomeMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    public class WelcomeMessage
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("=====================================");
+            Console.WriteLine("Welcome To Address Book");
+            Console.WriteLine("=====================================");
+        }
+
+        public static void DisplayMsg()
+        {
+            Console.WriteLine("Please Select Below \n Press 1 to Add Contact ");
+            Console.WriteLine("Press 2 to Remove Contact");
+            Console.WriteLine("Press 3 to view Contact");
+        }
+    }
+}
